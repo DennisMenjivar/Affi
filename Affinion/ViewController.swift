f72c025116b0c932e5d220f65901612868664148
Tatiana <DennisMenjivar@denniss-macbook-pro.local>
2016-03-22 16:19:48 -0600

    Initial Commit

---
+++ Affinion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Affinion
+//
+//  Created by Dennis Menjivar on 3/22/16.
+//  Copyright © 2016 Dennis Menjivar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
